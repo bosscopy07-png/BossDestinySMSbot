2ff5e13d45730a3bd2928c0e096f19c30927aec5
bosscopy07-png <bosscopy07@gmail.com>
2026-04-24 23:22:32 +0100

    Create index.js

---
+++ models/index.js
@@ -0,0 +1,17 @@
+import User from './User.js';
+import Session from './Session.js';
+import Transaction from './Transaction.js';
+import Number from './Number.js';
+import Referral from './Referral.js';
+import ApiKey from './ApiKey.js';
+import AdminLog from './AdminLog.js';
+
+export {
+    User,
+    Session,
+    Transaction,
+    Number,
+    Referral,
+    ApiKey,
+    AdminLog
+};
