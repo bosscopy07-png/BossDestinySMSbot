8f93495cbfe4a12319d666d47b062303d7ad4f51
bosscopy07-png <bosscopy07@gmail.com>
2026-04-25 15:24:03 +0100

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 import User from './User.js';
 import Session from './Session.js';
 import Transaction from './Transaction.js';
-import Number from './Number.js';
+import NumberModel from './Number.js';
 import Referral from './Referral.js';
 import ApiKey from './ApiKey.js';
 import AdminLog from './AdminLog.js';
@@ -10,7 +10,7 @@ export {
     User,
     Session,
     Transaction,
-    Number,
+    NumberModel as Number,
     Referral,
     ApiKey,
     AdminLog
