b391b910255195d7de4f9c3dd8cda665c1e444a6
bosscopy07-png <bosscopy07@gmail.com>
2026-04-29 00:42:18 +0100

    Update buy-numbers.js

--- services/sms/buy-numbers.js
+++ services/sms/buy-numbers.js
@@ -2,7 +2,7 @@
 import NumberPoolManager from './NumberPoolManager.js';
 import TwilioProvider from './TwilioProvider.js';
 import { connectDB, disconnectDB } from '../../../config/database.js';
-import logger from '../../../utils/logger.js';
+import logger from '../../utils/logger.js';
 
 class BatchNumberBuyer {
     constructor(poolManager) {
